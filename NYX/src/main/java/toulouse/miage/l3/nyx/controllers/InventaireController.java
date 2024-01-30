e30f1cfde904e1765002aca1208f2200f96ecd9d
Hideaki223 <lucas.godard@proton.me>
2024-01-30 08:24:43 +0100

    création de la classe InventaireController et de sa page fxml

---
+++ NYX/src/main/java/toulouse/miage/l3/nyx/controllers/InventaireController.java
@@ -0,0 +1,20 @@
+package toulouse.miage.l3.nyx.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import toulouse.miage.l3.nyx.core.model.Element;
+
+import javax.swing.text.TableView;
+import java.io.IOException;
+import java.util.Objects;
+
+public class InventaireController {
+    @FXML
+    private TableView elementTableView;
+
+}
