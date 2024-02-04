e2e174ec9a39989e7718a83f21c170c303159794
AzureAD\HuguesAnsoborlo <hugues.ansoborlo@toulouse.miage.fr>
2024-02-04 19:59:44 +0100

    donnees tableau ajout split panel

--- NYX/src/main/java/toulouse/miage/l3/nyx/controllers/ChaineController.java
+++ NYX/src/main/java/toulouse/miage/l3/nyx/controllers/ChaineController.java
@@ -1,20 +1,65 @@
 package toulouse.miage.l3.nyx.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.cell.*;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+import toulouse.miage.l3.nyx.core.model.Chaine;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class ChaineController {
+import static toulouse.miage.l3.nyx.core.model.Usine.*;
+public class ChaineController implements Initializable {
 
+    @FXML
+    private TableView<Chaine> chaineTableView;
+    @FXML
+    private TableColumn<Chaine, Integer> chaineCode;
+    @FXML
+    private TableColumn<Chaine, String> chaineNom;
+    @FXML
+    private TableColumn<Chaine, String> chaineEntree;
+    @FXML
+    private TableColumn<Chaine, String> chaineSortie;
+    @FXML
     private Stage stage;
     private Scene scene;
     private Parent root;
+    private int currentValue;
+
+    private HashMap<Chaine, Integer> chaineQuantities = new HashMap<>();
+
+    public void initialize(URL location, ResourceBundle resources) {
+        currentValue = 0;
+        chaineCode.setCellValueFactory(new PropertyValueFactory<>("code"));
+        chaineNom.setCellValueFactory(new PropertyValueFactory<>("nom"));
+        chaineEntree.setCellValueFactory(new PropertyValueFactory<>("listeElementEntree"));
+        chaineSortie.setCellValueFactory(new PropertyValueFactory<>("listeElementSortie"));
+
+        Callback<TableColumn<Chaine, String>, TableCell<Chaine, String>> cellFoctory = (TableColumn<Chaine, String> param) -> {
+            final TableCell<Chaine, String> cell = new TableCell<Chaine, String>() {};
+            return cell;
+        };
+        chaineTableView.setItems(listesChaines);
+    }
+
+    public HashMap<Chaine, Integer> getChaineQuantities() {
+        return chaineQuantities;
+    }
 
 
 
---
+++ NYX/src/main/java/toulouse/miage/l3/nyx/controllers/InventaireController.java
@@ -0,0 +1,82 @@
+package toulouse.miage.l3.nyx.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import toulouse.miage.l3.nyx.core.model.Element;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+import static toulouse.miage.l3.nyx.core.model.Usine.listesChaines;
+import static toulouse.miage.l3.nyx.core.model.Usine.listesElements;
+
+public class InventaireController implements Initializable {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private TableView<Element> elementTableView;
+    @FXML
+    private TableColumn<Element, Integer> elementCode;
+    @FXML
+    private TableColumn<Element, String> elementNom;
+    @FXML
+    private TableColumn<Element, Double> elementPrixA;
+    @FXML
+    private TableColumn<Element, Double> elementPrixV;
+    @FXML
+    private TableColumn<Element, Double> elementQuantite;
+    @FXML
+    private TableColumn<Element, String> elementUnite;
+
+    public void goToAccueil(javafx.event.ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/toulouse/miage/l3/nyx/fxml/accueil-view.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        String css = this.getClass().getResource("/toulouse/miage/l3/nyx/style/accueil.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void goToChaineProduction(javafx.event.ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/toulouse/miage/l3/nyx/fxml/chaine-view.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        String css = this.getClass().getResource("/toulouse/miage/l3/nyx/style/chaine.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void goToInventaire(ActionEvent actionEvent) throws IOException {
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/toulouse/miage/l3/nyx/fxml/inventaire-view.fxml")));
+        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        String css = this.getClass().getResource("/toulouse/miage/l3/nyx/style/inventaire.css").toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        elementCode.setCellValueFactory(new PropertyValueFactory<>("code"));
+        elementNom.setCellValueFactory(new PropertyValueFactory<>("nom"));
+        elementPrixA.setCellValueFactory(new PropertyValueFactory<>("prixAchat"));
+        elementPrixV.setCellValueFactory(new PropertyValueFactory<>("prixVente"));
+        elementQuantite.setCellValueFactory(new PropertyValueFactory<>("quantite"));
+        elementUnite.setCellValueFactory(new PropertyValueFactory<>("uniteMesure"));
+        elementTableView.setItems(listesElements);
+    }
+}
