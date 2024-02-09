c30d68c50a1db671dff5e73e9af689f7c440c238
lucas-godard <lucas.godard@proton.me>
2024-02-09 14:36:07 +0100

    Javadoc Element / InventaireController

--- NYX/src/main/java/toulouse/miage/l3/nyx/controllers/InventaireController.java
+++ NYX/src/main/java/toulouse/miage/l3/nyx/controllers/InventaireController.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static toulouse.miage.l3.nyx.core.model.Usine.addToElements;
 import static toulouse.miage.l3.nyx.core.model.Usine.elements;
 
 public class InventaireController implements Initializable {
@@ -48,6 +49,19 @@ public class InventaireController implements Initializable {
     @FXML
     private TextField ajoutunite;
 
+    /**
+     * The application will load this function at the start when it's called
+     *
+     * The code will display a table view, with custom columns
+     *
+     * @param url
+     * The location used to resolve relative paths for the root object, or
+     * {@code null} if the location is not known.
+     *
+     * @param resourceBundle
+     * The resources used to localize the root object, or {@code null} if
+     * the root object was not localized.
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         elementCode.setCellValueFactory(new PropertyValueFactory<>("code"));
@@ -59,13 +73,17 @@ public class InventaireController implements Initializable {
         elementTableView.setItems(elements);
     }
 
+    /**
+     * Enable to change the scene from inventaire to acceuil
+     * @param actionEvent - click
+     */
     public void goToAccueil(ActionEvent actionEvent) throws IOException {
         SceneUtils.goToScene("/toulouse/miage/l3/nyx/fxml/accueil-view.fxml",
                 "/toulouse/miage/l3/nyx/style/accueil.css", actionEvent);
     }
 
     /**
-     *
+     * Enable to change the scene from inventaire to chaine de production
      * @param actionEvent
      */
     public void goToChaineProduction(ActionEvent actionEvent) throws IOException {
@@ -74,7 +92,7 @@ public class InventaireController implements Initializable {
     }
 
     /**
-     *
+     * Enable to change the scene from inventaire to inventaire (refresh)
      * @param actionEvent
      */
     public void goToInventaire(ActionEvent actionEvent) throws IOException {
@@ -83,17 +101,17 @@ public class InventaireController implements Initializable {
     }
 
     /**
-     *
+     * Create an element and add it in the table view
      */
     public void addElement(){
         Element e = new Element(ajoutcode.getText(),ajoutnom.getText(),
                 Double.parseDouble(ajoutqte.getText()), ajoutunite.getText(),
                 Double.parseDouble(ajoutprixa.getText()), Double.parseDouble(ajoutprixv.getText()));
-        elements.add(e);
+        addToElements(e);
     }
 
     /**
-     *
+     * Remove the selected element from the table view
      */
     public void delElement(){
         Element e = elementTableView.getSelectionModel().getSelectedItem();
--- NYX/src/main/java/toulouse/miage/l3/nyx/core/model/Element.java
+++ NYX/src/main/java/toulouse/miage/l3/nyx/core/model/Element.java
@@ -1,12 +1,34 @@
 package toulouse.miage.l3.nyx.core.model;
+
+/**
+ * Class Element
+ *
+ * @author Lucas Godard
+ * @version 1.0
+ */
 public class Element {
+    /** code of the element */
     private String code;
+    /** name of the element */
     private String nom;
+    /** quantity of the element */
     private Double quantite;
+    /** unit of the element */
     private String uniteMesure;
+    /** purchase price of element */
     private Double prixAchat;
+    /** selling price of element */
     private Double prixVente;
 
+    /**
+     * Constructeur de l'element
+     * @param code
+     * @param nom
+     * @param quantite
+     * @param uniteMesure
+     * @param prixAchat
+     * @param prixVente
+     */
     public Element(String code, String nom, Double quantite, String uniteMesure, Double prixAchat, Double prixVente) {
         this.code = code;
         this.uniteMesure = uniteMesure;
@@ -16,42 +38,74 @@ public class Element {
         this.nom = nom;
     }
 
-    public Element() {
-
-    }
-
+    /**
+     * Get the element's code
+     * @return code
+     */
     public String getCode() {
         return code;
     }
 
+    /**
+     * Get the element's purchase price
+     * @return prixAchat
+     */
     public Double getPrixAchat() {
         return prixAchat;
     }
 
+    /**
+     * Set the element's purchase price
+     * @param prixAchat
+     */
     public void setPrixAchat(Double prixAchat) {
         this.prixAchat = prixAchat;
     }
 
+    /**
+     * Get the element's selling price
+     * @return prixVente
+     */
     public Double getPrixVente() {
         return prixVente;
     }
 
+    /**
+     * Set the element's selling price
+     * @param prixVente
+     */
     public void setPrixVente(Double prixVente) {
         this.prixVente = prixVente;
     }
 
+    /**
+     * Get the element's name
+     * @return nom
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Get the element's quantity
+     * @return quantite
+     */
     public Double getQuantite() {
         return quantite;
     }
 
+    /**
+     * Get the element's unit
+     * @return uniteMesure
+     */
     public String getUniteMesure() {
         return uniteMesure;
     }
 
+    /**
+     * Set the element's unit
+     * @param quantite
+     */
     public void setQuantite(Double quantite) {
         this.quantite = quantite;
     }
